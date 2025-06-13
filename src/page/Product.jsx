f6512561f29b6fc42bb3ae93314bda541a2ab3c0
Steen hjalmar larsen <sthl@youmail.dk>
2025-06-13 11:34:43 +0200

    price

--- src/page/Product.jsx
+++ src/page/Product.jsx
@@ -22,9 +22,9 @@ export default function Product() {
                     return <li key={product.model}>
                         <h2>brand:{product.brand}</h2>
                         <ul>
+                            <li>Pris {product.price_dkk}</li>
                             <li>model {product.model}</li>
                             <li>type {product.type}</li>
-                            <li>power_output {product.power_output}</li>
                             <li>power_output {product.power_output}</li>
                         </ul>
                     </li>
