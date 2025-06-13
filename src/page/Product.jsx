ac7969a21db879b81f1194cae68974da9e974c65
Steen hjalmar larsen <sthl@youmail.dk>
2025-06-13 11:15:18 +0200

    commit-test-01

--- src/page/Product.jsx
+++ src/page/Product.jsx
@@ -2,22 +2,31 @@ import { useEffect, useState } from "react"
 
 export default function Product() {
 
+
+
     const [procutsData, setProductsData] = useState([])
     const url = "http://localhost:4000"
-    fetch(`${url}/products`)
-        .then(respons => { return respons.json() })
-        .then(procutsData => { setProductsData(procutsData) })
+    useEffect(() => {
+        fetch(`${url}/products`)
+            .then(respons => { return respons.json() })
+            .then(procutsData => { setProductsData(procutsData) })
 
-    // useEffect(() => {
-    //     console.log(procutsData);
+    }, [])
 
-    // }, [])
+
     return (
         <section>
             <h1>Product</h1>
             <ul>
                 {procutsData && procutsData.map((product) => {
-                    return <li key={product.model}>{product.brand}</li>
+                    return <li key={product.model}>
+                        <h2>brand:{product.brand}</h2>
+                        <ul>
+                            <li>model {product.model}</li>
+                            <li>type {product.type}</li>
+                            <li>power_output {product.power_output}</li>
+                        </ul>
+                    </li>
                 })}
             </ul>
         </section>
